885a18af608613ea27060930c67ab6dd3b895791
Daniel Neshkoski <limun@Daniels-MacBook-Pro.local>
2021-01-09 20:54:51 +0100

    build for pages

---
+++ build/precache-manifest.3e36a5a42a189d54b8bbbc16201afe36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f87ccb30d0c02f1da1d64cf98966dac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74a27fe72d08e2e22671",
+    "url": "/static/css/main.f38965af.chunk.css"
+  },
+  {
+    "revision": "310a8218b6a613766954",
+    "url": "/static/js/2.936f6cad.chunk.js"
+  },
+  {
+    "revision": "bd16ffa8a645b42adcb7fc65d4c790f8",
+    "url": "/static/js/2.936f6cad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74a27fe72d08e2e22671",
+    "url": "/static/js/main.ab485fea.chunk.js"
+  },
+  {
+    "revision": "142b697ed77b435eaccc",
+    "url": "/static/js/runtime-main.efa8e349.js"
+  }
+]);
